422af6ade6d4793a2496f78c4415599ecaad4a34
unknown <omahmoudali9@gmail.com>
2024-12-17 01:21:23 +0200

    search

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('' , views.home , name='home'),
-    path('<slug:cat_slug>/' , views.home , name='product_by_cat') , 
-    path('<slug:cat_slug>/<slug:slug_product>/' , views.product_detail , name ='detais_product' )
+    path('category/<slug:cat_slug>/' , views.home , name='product_by_cat') , 
+    path('category/<slug:cat_slug>/<slug:slug_product>/' , views.product_detail , name ='detais_product' ),
+    path('search/' , views.search , name='search')
 ]
--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-
+from django.core.paginator import EmptyPage , PageNotAnInteger , Paginator
 from django.shortcuts import render , get_object_or_404
 from .models import Product , Category
 from cart.models import Cart_Item,Cart
@@ -13,15 +13,22 @@ def home(request , cat_slug=None):
         cat = get_object_or_404(Category , slug = cat_slug)
         # get slug of catengury
         pro = Product.objects.filter(category=cat , is_available = True )
+        pagintor = Paginator(pro,2)
+        page= request.GET.get('page')
+        paged_product = pagintor.get_page(page)
         # filter the products with the categury and available product
         product_count = pro.count()
     else :    
         # if  you dont clicked give me products available
-        pro = Product.objects.all().filter(is_available = True)
+        pro = Product.objects.all().filter(is_available = True).order_by('id')
+        pagintor = Paginator(pro , 4)
+        page = request.GET.get('page')
+        paged_product = pagintor.get_page(page)
+        #link the pagintaor have limit products and all products with get_page function take page parameter
         product_count = pro.count()
 
     context = {
-        'products' : pro,
+        'products' : paged_product,
         'product_count' : product_count
 
         }
@@ -40,4 +47,20 @@ def product_detail(request, cat_slug, slug_product):
         'in_cart' : in_cart
     }
     return render(request, 'store_html/product-detail.html' , context   )
+
+def search(request):
+    keyword = request.GET.get('keyword', '')  # Get 'keyword' from the request, default to an empty string
+    products = []  # Default to an empty list
+    product_count = 0  # Default count is 0
+
+    if keyword:  # Proceed only if keyword is not empty
+        products = Product.objects.filter(name__icontains=keyword).order_by('-created_date')
+        product_count = products.count() 
+        
+    context = {
+            'products' : products,
+            'product_count' : product_count
+    }    
+    return render(request , 'store_html/home.html' , context)
+
 
